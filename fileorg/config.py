4f1a6d4c755bcef82c8cf8c6cb036072d37d198a
João Sá <joaopedrosavieira@gmail.com>
2026-01-16 16:11:57 +0100

    Normalização das extensões e inversão das regras
    Por motivos de eficiência, o dicionário com as regras irá guarda-las no formato (extensão -> categoria) em vez do formato (categoria -> extensão).
    Além disso, durante esse proceso, todas as extensões irão ser verificadas para normalizar para o mesmo formato, que é sem ponto e em minúsculas.

--- fileorg/config.py
+++ fileorg/config.py
@@ -2,7 +2,7 @@ import json
 
 # Definição das regras padrão em caso de não especificação por parte do utilizador
 DEFAULT_RULES = {
-    "Imagens": ["jpg", "jpeg", "png", "gif", "webp"],
+    "Imagens": [".jpg", "jpeg", "png", "gif", "webp"],
     "Documentos": ["pdf", "docx", "txt", "md"],
     "Compactados": ["zip", "rar", "7z", "tar", "gz"],
     "Audio": ["mp3", "wav", "flac"],
@@ -11,8 +11,26 @@ DEFAULT_RULES = {
     "Other": []
 }
 
+# Escolhe entre a configuração padrão ou a escolhida pelo utilizador
 def load_config(path=None):
     if path:
         with open(path, "r") as f:
-            return json.load(f)
-    return DEFAULT_RULES
+            return invert_rules(json.load(f))
+    return invert_rules(DEFAULT_RULES)
+
+# Inverte as regras para aumentar a eficiencia durante a organização dos ficheiros
+def invert_rules(rules):
+    extension_map = {}
+
+    for category, extensions in rules.items():
+        for ext in extensions:
+            # Se tiver um ponto irá ser removido e se tiver letras maiúsculas irá ser trocado por letras minúsculas
+            if (ext[0] == "." or ext.isupper()):
+                extension_map[ext[1:].lower()] = category
+            else:
+                extension_map[ext] = category
+    return extension_map
+
+
+# TODO - Adicionar uma função para verificar se a configuração indicada pelo utilizador
+# é valida, ou seja, se não tem a mesma extensão em duas categorias diferentes, etc.
